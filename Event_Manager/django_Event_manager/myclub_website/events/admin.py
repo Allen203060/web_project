57c3e66a60e058bd51df98fd4c1b15a30bc44dd1
Allen <allenmanoj2006@gmail.com>
2025-04-10 10:10:55 +0530

    deleted files

--- Event_Manager/django_Event_manager/myclub_website/events/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Venue
-from .models import MyClubUser
-from .models import Event
-
-
-admin.site.register(Venue)
-admin.site.register(MyClubUser)
-admin.site.register(Event)
